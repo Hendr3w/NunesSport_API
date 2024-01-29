1f7eb1aaa0ddba664300d30a2d4c9e5c8f430917
Hendr3w <hendreewgustavo@gmail.com>
2024-01-28 23:52:22 -0300

    First Commit

---
+++ NunesSports_Back-End/src/Data/NunesSportsDbContext.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using NunesSport_Back_End.src.Models;
+
+namespace NunesSport_Back_End.src.Data
+{
+    public class NunesSportsDbContext : DbContext
+    {
+        public DbSet<Produto> Produto { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = "Data Source=nunesSports.db";
+
+            optionsBuilder.UseSqlite(connectionString);
+        }
+    }
+    
+}
